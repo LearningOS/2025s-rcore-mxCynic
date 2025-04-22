09e9a1cf282abad5f3059d14d583fa8f221d8701
mxCynic <mx1545914198@arch>
2025-04-22 22:33:01 +0800

    update

--- os/src/mm/mod.rs
+++ os/src/mm/mod.rs
@@ -11,10 +11,9 @@ mod frame_allocator;
 mod heap_allocator;
 mod memory_set;
 mod page_table;
-
 pub use address::{PhysAddr, PhysPageNum, VirtAddr, VirtPageNum};
 use address::{StepByOne, VPNRange};
-pub use frame_allocator::{frame_alloc, FrameTracker};
+pub use frame_allocator::{frame_alloc, frame_dealloc, FrameTracker};
 pub use memory_set::remap_test;
 pub use memory_set::{kernel_stack_position, MapPermission, MemorySet, KERNEL_SPACE};
 pub use page_table::{translated_byte_buffer, PageTableEntry};
